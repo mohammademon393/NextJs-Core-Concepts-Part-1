83464171d7232bcac23f10dd8cde8ab004c747b2
mohammademon393 <mdemon357ep@gmail.com>
2026-09-17 11:15:36 +0600

    Conditional layout emplement

--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -1,19 +1,39 @@
+"use client"
+import { MonitorCog } from 'lucide-react';
 import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 import React from 'react';
 
 const Header = () => {
+  const pathname = usePathname();
+  if(pathname.startsWith('/dashboard')) return <></>;
     return (
-        <header className="bg-blue-900 text-white p-4 flex items-center justify-between flex-wrap">
-          <Link href="/" className="text-2xl font-semibold">Dev-Story</Link>
+      <header className="bg-blue-900 text-white p-4 flex items-center justify-between flex-wrap">
+        <div className='flex gap-2 items-center'>
+          <MonitorCog />
+          <Link href="/" className="text-2xl font-semibold">
+            Dev-Story
+          </Link>
+        </div>
 
-          <nav className="flex space-x-4">
-            <Link href="/tutorials" className="hover:underline">Tutorials</Link>
-            <Link href="/about" className="hover:underline">About</Link>
-            <Link href="/stories" className="hover:underline">Stories</Link>
-            <Link href="/login" className="hover:underline">Login</Link>
-            <Link href="/register" className="hover:underline">Register</Link>
-          </nav>
-        </header>
+        <nav className="flex space-x-4">
+          <Link href="/tutorials" className="hover:underline">
+            Tutorials
+          </Link>
+          <Link href="/about" className="hover:underline">
+            About
+          </Link>
+          <Link href="/stories" className="hover:underline">
+            Stories
+          </Link>
+          <Link href="/login" className="hover:underline">
+            Login
+          </Link>
+          <Link href="/register" className="hover:underline">
+            Register
+          </Link>
+        </nav>
+      </header>
     );
 };
 
--- src/app/dashboard/layout.jsx
+++ src/app/dashboard/layout.jsx
@@ -1,28 +1,40 @@
+import { MonitorCog } from 'lucide-react';
 import Link from 'next/link';
 import React from 'react';
 
 const DashboardLayout = ({ children }) => {
     return (
-        <div className = "grid grid-cols-12 gap-5 min-h-screen">
-            <div className = "col-span-3 border-r-2">
-                <h2 className = "font-bold text-2xl mb-4 mx-2">Navigation</h2>
-
-                <div className = "flex flex-col space-y-4 mt-5">
-                    <Link href="/dashboard/add-story" className="py-2 px-4 mx-2 bg-blue-900 text-white rounded text-center hover:bg-black  transition duration-300">
-                        Add Story
-                    </Link>
-                    <Link href="/dashboard/my-profile" className="py-2 px-4 mx-2 bg-blue-900 text-white rounded text-center hover:bg-black  transition duration-300">
-                        My Profile
-                    </Link>
-                    <Link href="/dashboard/settings" className="py-2 px-4 mx-2 bg-blue-900 text-white rounded text-center hover:bg-black  transition duration-300">
-                        Settings
-                    </Link>
-                </div>
-            </div>
-            <div className = "col-span-9">
-                {children}
-            </div>
+      <div className="grid grid-cols-12 gap-5 min-h-screen">
+        <div className="col-span-3 border-r-2">
+          <div className='flex gap-2 items-center'>
+            <MonitorCog />
+            <Link href="/" className="text-2xl font-semibold">
+              Dev-Story
+            </Link>
+          </div>
+          <div className="flex flex-col space-y-4 mt-5">
+            <Link
+              href="/dashboard/add-story"
+              className="py-2 px-4 mx-2 bg-blue-900 text-white rounded text-center hover:bg-black  transition duration-300"
+            >
+              Add Story
+            </Link>
+            <Link
+              href="/dashboard/my-profile"
+              className="py-2 px-4 mx-2 bg-blue-900 text-white rounded text-center hover:bg-black  transition duration-300"
+            >
+              My Profile
+            </Link>
+            <Link
+              href="/dashboard/settings"
+              className="py-2 px-4 mx-2 bg-blue-900 text-white rounded text-center hover:bg-black  transition duration-300"
+            >
+              Settings
+            </Link>
+          </div>
         </div>
+        <div className="col-span-9">{children}</div>
+      </div>
     );
 };
 
